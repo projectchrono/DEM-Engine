38aaa52ec65f325835abc137d515f7bc71190580
Ruochun Zhang <ruochunz@gmail.com>
2023-01-14 19:00:51 -0600

    Testing GRC iter2 DP

--- src/demo/DEMdemo_WheelDP.cpp
+++ src/demo/DEMdemo_WheelDP.cpp
@@ -20,7 +20,7 @@ const double math_PI = 3.1415927;
 
 int main() {
     std::filesystem::path out_dir = std::filesystem::current_path();
-    out_dir += "/DEMdemo_WheelDP_Force_mu0.9_750N";
+    out_dir += "/DEMdemo_WheelDP_Force_mu0.5_750N";
     std::filesystem::create_directory(out_dir);
 
     // `World'
@@ -41,7 +41,7 @@ int main() {
     float wheel_IXX = (wheel_mass / 12) * (3 * wheel_rad * wheel_rad + wheel_width * wheel_width);
 
     // float TRs[] = {0.1, 0.05};
-    float TRs[] = {0.9, 0.7, 0.5, 0.3, 0.2, 0.1, 0.05};
+    float TRs[] = {0.7, 0.5, 0.3, 0.2, 0.1, 0.05, 0};
     unsigned int run_mode = 0;
     unsigned int currframe = 0;
 
@@ -54,15 +54,16 @@ int main() {
         DEMSim.SetContactOutputContent(OWNER | FORCE | POINT);
 
         // E, nu, CoR, mu, Crr...
-        auto mat_type_wheel = DEMSim.LoadMaterial({{"E", 1e9}, {"nu", 0.3}, {"CoR", 0.5}, {"mu", 0.9}, {"Crr", 0.00}});
+        auto mat_type_wall = DEMSim.LoadMaterial({{"E", 1e9}, {"nu", 0.3}, {"CoR", 0.5}, {"mu", 0.9}, {"Crr", 0.00}});
+        auto mat_type_wheel = DEMSim.LoadMaterial({{"E", 1e9}, {"nu", 0.3}, {"CoR", 0.5}, {"mu", 0.5}, {"Crr", 0.00}});
         auto mat_type_terrain =
-            DEMSim.LoadMaterial({{"E", 1e9}, {"nu", 0.3}, {"CoR", 0.5}, {"mu", 0.9}, {"Crr", 0.00}});
+            DEMSim.LoadMaterial({{"E", 1e9}, {"nu", 0.3}, {"CoR", 0.5}, {"mu", 0.5}, {"Crr", 0.00}});
 
         DEMSim.InstructBoxDomainDimension(world_size_x, world_size_y, world_size_z);
-        DEMSim.InstructBoxDomainBoundingBC("top_open", mat_type_terrain);
+        DEMSim.InstructBoxDomainBoundingBC("top_open", mat_type_wall);
         DEMSim.SetCoordSysOrigin("center");
         float bottom = -0.5;
-        auto bot_wall = DEMSim.AddBCPlane(make_float3(0, 0, bottom), make_float3(0, 0, 1), mat_type_terrain);
+        auto bot_wall = DEMSim.AddBCPlane(make_float3(0, 0, bottom), make_float3(0, 0, 1), mat_type_wall);
         auto bot_wall_tracker = DEMSim.Track(bot_wall);
 
         auto wheel = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile("mesh/rover_wheels/curiosity_wheel_surface.obj"),
@@ -74,24 +75,32 @@ int main() {
         // Track it
         auto wheel_tracker = DEMSim.Track(wheel);
 
-        // Define the GRC terrain particle templates
-        DEMClumpTemplate shape_template;
-        shape_template.ReadComponentFromFile(GetDEMEDataFile("clumps/triangular_flat.csv"));
+        // Then the ground particle template
+        DEMClumpTemplate shape_template1, shape_template2;
+        shape_template1.ReadComponentFromFile((GET_DATA_PATH() / "clumps/triangular_flat.csv").string());
+        shape_template2.ReadComponentFromFile((GET_DATA_PATH() / "clumps/triangular_flat_6comp.csv").string());
+        std::vector<DEMClumpTemplate> shape_template = {shape_template2, shape_template2, shape_template1,
+                                                        shape_template1, shape_template1, shape_template1,
+                                                        shape_template1};
         // Calculate its mass and MOI
-        float terrain_density = 2.6e3;
-        double clump_vol = 5.5886717;
-        float mass = terrain_density * clump_vol;  // in kg or g
-        float3 MOI = make_float3(1.8327927, 2.1580013, 0.77010059) * (double)2.6e3;
+        float mass1 = 2.6e3 * 5.5886717;  // in kg or g
+        float3 MOI1 = make_float3(1.8327927, 2.1580013, 0.77010059) * 2.6e3;
+        float mass2 = 2.6e3 * 2.7564385;  // in kg or g
+        float3 MOI2 = make_float3(1.0352626, 0.9616627, 1.6978352) * 2.6e3;
+        std::vector<float> mass = {mass2, mass2, mass1, mass1, mass1, mass1, mass1};
+        std::vector<float3> MOI = {MOI2, MOI2, MOI1, MOI1, MOI1, MOI1, MOI1};
         // Scale the template we just created
         std::vector<std::shared_ptr<DEMClumpTemplate>> ground_particle_templates;
-        std::vector<double> scales = {0.00063, 0.00033, 0.00022, 0.00015, 0.00009};
-        std::for_each(scales.begin(), scales.end(), [](double& r) { r *= 20.; });
+        std::vector<double> volume = {2.7564385, 2.7564385, 5.5886717, 5.5886717, 5.5886717, 5.5886717, 5.5886717};
+        std::vector<double> scales = {0.0014, 0.00075833, 0.00044, 0.0003, 0.0002, 0.00018333, 0.00017};
+        std::for_each(scales.begin(), scales.end(), [](double& r) { r *= 10.; });
+        unsigned int t_num = 0;
         for (double scaling : scales) {
-            auto this_template = shape_template;
-            this_template.mass = (double)mass * scaling * scaling * scaling;
-            this_template.MOI.x = (double)MOI.x * (double)(scaling * scaling * scaling * scaling * scaling);
-            this_template.MOI.y = (double)MOI.y * (double)(scaling * scaling * scaling * scaling * scaling);
-            this_template.MOI.z = (double)MOI.z * (double)(scaling * scaling * scaling * scaling * scaling);
+            auto this_template = shape_template[t_num];
+            this_template.mass = (double)mass[t_num] * scaling * scaling * scaling;
+            this_template.MOI.x = (double)MOI[t_num].x * (double)(scaling * scaling * scaling * scaling * scaling);
+            this_template.MOI.y = (double)MOI[t_num].y * (double)(scaling * scaling * scaling * scaling * scaling);
+            this_template.MOI.z = (double)MOI[t_num].z * (double)(scaling * scaling * scaling * scaling * scaling);
             std::cout << "Mass: " << this_template.mass << std::endl;
             std::cout << "MOIX: " << this_template.MOI.x << std::endl;
             std::cout << "MOIY: " << this_template.MOI.y << std::endl;
@@ -102,15 +111,20 @@ int main() {
             std::for_each(this_template.relPos.begin(), this_template.relPos.end(),
                           [scaling](float3& r) { r *= scaling; });
             this_template.materials = std::vector<std::shared_ptr<DEMMaterial>>(this_template.nComp, mat_type_terrain);
-            this_template.SetVolume(clump_vol * scaling * scaling * scaling);
+
+            // Give these templates names, 0000, 0001 etc.
+            char t_name[20];
+            sprintf(t_name, "%04d", t_num);
+            this_template.AssignName(std::string(t_name));
             ground_particle_templates.push_back(DEMSim.LoadClumpType(this_template));
+            t_num++;
         }
 
         // Now we load clump locations from a checkpointed file
         {
             std::cout << "Making terrain..." << std::endl;
-            auto clump_xyz = DEMSim.ReadClumpXyzFromCsv("./GRC_20e6.csv");
-            auto clump_quaternion = DEMSim.ReadClumpQuatFromCsv("./GRC_20e6.csv");
+            auto clump_xyz = DEMSim.ReadClumpXyzFromCsv("../build_3/GRC_20e6.csv");
+            auto clump_quaternion = DEMSim.ReadClumpQuatFromCsv("../build_3/GRC_20e6.csv");
             std::vector<float3> in_xyz;
             std::vector<float4> in_quat;
             std::vector<std::shared_ptr<DEMClumpTemplate>> in_types;
@@ -142,8 +156,8 @@ int main() {
             // Now, we don't need all particles loaded...
             std::vector<notStupidBool_t> elem_to_remove(in_xyz.size(), 0);
             for (size_t i = 0; i < in_xyz.size(); i++) {
-                if (std::abs(in_xyz.at(i).y) > (world_size_y - 0.03) / 2 ||
-                    std::abs(in_xyz.at(i).x) > (world_size_x - 0.06) / 2)
+                if (std::abs(in_xyz.at(i).y) > (world_size_y - 0.05) / 2 ||
+                    std::abs(in_xyz.at(i).x) > (world_size_x - 0.04) / 2)
                     elem_to_remove.at(i) = 1;
             }
             in_xyz.erase(std::remove_if(in_xyz.begin(), in_xyz.end(),
@@ -222,7 +236,7 @@ int main() {
         DEMSim.SetCDUpdateFreq(20);
         DEMSim.SetExpandSafetyAdder(0.5);
         DEMSim.SetMaxVelocity(40);
-        DEMSim.SetInitBinSize(2 * scales.at(2));
+        DEMSim.SetInitBinSizeAsMultipleOfSmallestSphere(2);
         DEMSim.Initialize();
 
         // Compress until dense enough
@@ -277,7 +291,7 @@ int main() {
                 DEMSim.ShowThreadCollaborationStats();
             }
 
-            if (t >= 4. && !start_measure) {
+            if (t >= 3. && !start_measure) {
                 start_measure = true;
             }
 
