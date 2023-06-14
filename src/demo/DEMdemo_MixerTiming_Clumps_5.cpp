f1a879af5d2ab6fa7cfb02b3127a7c3647781f8d
Ruochun Zhang <ruochunz@gmail.com>
2023-06-14 12:20:40 -0500

    Clump5 max

--- src/demo/DEMdemo_MixerTiming_Clumps_5.cpp
+++ src/demo/DEMdemo_MixerTiming_Clumps_5.cpp
@@ -18,7 +18,7 @@ using namespace deme;
 using namespace std::filesystem;
 
 int main() {
-    float granular_rad = 0.0008;
+    float granular_rad = 0.00084;
     unsigned int num_particles = 0;
     double world_size = 1;
     double CDFreq = 25.1;
