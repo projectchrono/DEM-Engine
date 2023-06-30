6a97667e337786f84f51f8b8db0f0c6c76b05cd1
Ruochun Zhang <ruochunz@gmail.com>
2023-06-29 23:58:02 -0500

    Test 5

--- src/demo/DEMdemo_Meshed_WheelDP_SlopeSlip_Earth.cpp
+++ src/demo/DEMdemo_Meshed_WheelDP_SlopeSlip_Earth.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[]) {
     int cur_test = atoi(argv[1]);
 
     std::filesystem::path out_dir = std::filesystem::current_path();
-    out_dir += "/DEMdemo_Wheel_Tests_4";
+    out_dir += "/DEMdemo_Wheel_Tests_5";
     std::filesystem::create_directory(out_dir);
 
     // `World'
