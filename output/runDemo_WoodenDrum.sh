e43eee1dd5bec512ec1065ad84a2d2f10e20d271
btagliafierro <btagliafierro@gmail.com>
2023-09-03 21:38:20 +0200

    post process for drum tests

--- output/runDemo_WoodenDrum.sh
+++ output/runDemo_WoodenDrum.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 counter=0
-for rolling_value in [0.00 0.01 0.02 0.04 0.08 0.16]; do
+for rolling_value in [0.00 0.01 0.02 0.04 0.08]; do
     ((counter++))
     echo "Iteration: $counter"
     ../build/src/demo/DEMdemo_Granular_WoodenCylinderDrum $counter $rolling_value
@@ -9,7 +9,7 @@ for rolling_value in [0.00 0.01 0.02 0.04 0.08 0.16]; do
 done
 
 counter=0
-for rolling_value in [0.00 0.01 0.02 0.04 0.08 0.16]; do
+for rolling_value in [0.00 0.01 0.02 0.04 0.08]; do
     ((counter++))
     echo "Iteration: $counter"
     ../build/src/demo/DEMdemo_Granular_WoodenSphereDrum $counter $rolling_value
