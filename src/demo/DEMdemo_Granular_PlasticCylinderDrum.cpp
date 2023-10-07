737b239f6871f36c1c8142609a7644fb45fab7b4
zeus <btagliafierro@gmail.com>
2023-10-07 16:09:13 +0200

    correct version of batches

--- src/demo/DEMdemo_Granular_PlasticCylinderDrum.cpp
+++ src/demo/DEMdemo_Granular_PlasticCylinderDrum.cpp
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
     
     
  
-    if ( argc =~ 5) {
+    if ( argc != 5) {
         printf("You have entered %d arguments, which is wrong!\n", argc);
         return 0;
     }
