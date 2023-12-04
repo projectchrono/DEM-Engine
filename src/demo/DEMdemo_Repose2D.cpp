ac0c48df5eab50a0e54a0cbd71595a718c208586
zeus <btagliafierro@gmail.com>
2023-12-04 23:09:12 +0100

    files for 2D simulations

---
+++ src/demo/DEMdemo_Repose2D.cpp
@@ -0,0 +1,183 @@
+//  Copyright (c) 2021, SBEL GPU Development Team
+//  Copyright (c) 2021, University of Wisconsin - Madison
+//
+//	SPDX-License-Identifier: BSD-3-Clause
+
+// =============================================================================
+// A repose angle test. Particles flow through a mesh-represented funnel and form
+// a pile that has an apparent angle.
+// =============================================================================
+
+#include <core/ApiVersion.h>
+#include <core/utils/ThreadManager.h>
+#include <DEM/API.h>
+#include <DEM/HostSideHelpers.hpp>
+#include <DEM/utils/Samplers.hpp>
+
+#include <cstdio>
+#include <chrono>
+#include <filesystem>
+
+using namespace deme;
+using namespace std::filesystem;
+
+int main() {
+    DEMSolver DEMSim;
+    DEMSim.UseFrictionalHertzianModel();
+    DEMSim.SetVerbosity(INFO);
+    DEMSim.SetOutputFormat(OUTPUT_FORMAT::CSV);
+
+    // If you don't need individual force information, then this option makes the solver run a bit faster.
+    DEMSim.SetNoForceRecord();
+
+    srand(42);
+
+    // Scale factor
+    float scaling = 2;
+
+    // total number of random clump templates to generate
+    int num_template = 6;
+
+    int min_sphere = 1;
+    int max_sphere = 5;
+
+    float min_rad = 0.01 * scaling;
+    float max_rad = 0.02 * scaling;
+
+    float min_relpos = -0.01 * scaling;
+    float max_relpos = 0.01 * scaling;
+
+    auto mat_type_walls = DEMSim.LoadMaterial({{"E", 1e8}, {"nu", 0.3}, {"CoR", 0.3}, {"mu", 1}});
+    auto mat_type_particles = DEMSim.LoadMaterial({{"E", 1e9}, {"nu", 0.3}, {"CoR", 0.7}, {"mu", 1}});
+    // If you don't have this line, then CoR between wall material and granular material will be 0.5 (average of the
+    // two).
+    DEMSim.SetMaterialPropertyPair("CoR", mat_type_walls, mat_type_particles, 0.3);
+
+    /*
+    // First create clump type 0 for representing the ground
+    float ground_sp_r = 0.02;
+    auto template_ground = DEMSim.LoadSphereType(0.5, ground_sp_r, mat_type_walls);
+    */
+
+    // Loaded meshes are by-default fixed
+    auto funnel = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile("mesh/funnel.obj"), mat_type_walls);
+    funnel->Scale(0.15);
+    float funnel_bottom = 0.f;
+
+    // Make an array to store these generated clump templates
+    std::vector<std::shared_ptr<DEMClumpTemplate>> clump_types;
+
+    // Then randomly create some clumps for piling up
+    for (int i = 0; i < num_template; i++) {
+        // first decide the number of spheres that live in this clump
+        int num_sphere = rand() % (max_sphere - min_sphere + 1) + 1;
+
+        // then allocate the clump template definition arrays (all in SI)
+        float mass = 0.1 * (float)num_sphere * std::pow(scaling, 3);
+        float3 MOI = make_float3(2e-5 * (float)num_sphere, 1.5e-5 * (float)num_sphere, 1.8e-5 * (float)num_sphere) *
+                     50. * std::pow(scaling, 5);
+        std::vector<float> radii;
+        std::vector<float3> relPos;
+
+        // randomly generate clump template configurations
+        // the relPos of a sphere is always seeded from one of the already-generated sphere
+        float3 seed_pos = make_float3(0);
+        for (int j = 0; j < num_sphere; j++) {
+            radii.push_back(((float)rand() / RAND_MAX) * (max_rad - min_rad) + min_rad);
+            float3 tmp;
+            if (j == 0) {
+                tmp.x = 0;
+                tmp.y = 0;
+                tmp.z = 0;
+            } else {
+                tmp.x = ((float)rand() / RAND_MAX) * (max_relpos - min_relpos) + min_relpos;
+                tmp.y = ((float)rand() / RAND_MAX) * (max_relpos - min_relpos) + min_relpos;
+                tmp.z = ((float)rand() / RAND_MAX) * (max_relpos - min_relpos) + min_relpos;
+            }
+            tmp += seed_pos;
+            relPos.push_back(tmp);
+
+            // seed relPos from one of the previously generated spheres
+            int choose_from = rand() % (j + 1);
+            seed_pos = relPos.at(choose_from);
+        }
+
+        // LoadClumpType returns a shared_ptr that points to this template so you may modify it. Also, material can be
+        // vector or a material shared ptr, and in the latter case it will just be applied to all component spheres this
+        // clump has.
+        auto clump_ptr = DEMSim.LoadClumpType(mass, MOI, radii, relPos, mat_type_walls);
+        clump_types.push_back(clump_ptr);
+    }
+
+    // Generate initial clumps for piling
+    float spacing = 0.08 * scaling;
+    float fill_width = 5.f;
+    float fill_height = 2.f * fill_width;
+    float fill_bottom = funnel_bottom + fill_width + spacing;
+    
+    PDSampler sampler(spacing);
+    // Use a PDSampler-based clump generation process
+    std::vector<std::shared_ptr<DEMClumpTemplate>> input_pile_template_type;
+    std::vector<float3> input_pile_xyz;
+    float layer_z = 0;
+    while (layer_z < fill_height) {
+        float3 sample_center = make_float3(0, 0, fill_bottom + layer_z + spacing / 2);
+        auto layer_xyz = sampler.SampleCylinderZ(sample_center, fill_width, 0);
+        unsigned int num_clumps = layer_xyz.size();
+        // Select from available clump types
+        for (unsigned int i = 0; i < num_clumps; i++) {
+            input_pile_template_type.push_back(clump_types.at(i % num_template));
+        }
+        input_pile_xyz.insert(input_pile_xyz.end(), layer_xyz.begin(), layer_xyz.end());
+        layer_z += spacing;
+    }
+    // Note: AddClumps can be called multiple times before initialization to add more clumps to the system
+    auto the_pile = DEMSim.AddClumps(input_pile_template_type, input_pile_xyz);
+
+    DEMSim.InstructBoxDomainDimension({-10, 10}, {-10, 10}, {funnel_bottom - 10.f, funnel_bottom + 20.f});
+    DEMSim.InstructBoxDomainBoundingBC("top_open", mat_type_walls);
+    DEMSim.SetInitTimeStep(5e-6);
+    DEMSim.SetGravitationalAcceleration(make_float3(0, 0, -9.81));
+    // Max velocity info is generally just for the solver's reference and the user do not have to set it. The solver
+    // wouldn't take into account a vel larger than this when doing async-ed contact detection: but this vel won't
+    // happen anyway and if it does, something already went wrong.
+    DEMSim.SetMaxVelocity(25.);
+    // You usually don't have to worry about initial bin size. In very rare cases, init bin size is so bad that auto bin
+    // size adaption is effectless, and you should notice in that case kT runs extremely slow. Then in that case setting
+    // init bin size may save the simulation.
+    // DEMSim.SetInitBinSize(min_rad * 6);
+
+    // 256 or 512 are common choices. Note that in cases where the force model is modified, too many registers may be
+    // used in the kernel, so we have to reduce this number to use 256. In other cases (and most cases), 512 is fine and
+    // may make the code run a bit faster. Usually, the user do not have to call SetForceCalcThreadsPerBlock if they
+    // don't know the implication.
+    DEMSim.SetForceCalcThreadsPerBlock(512);
+    DEMSim.Initialize();
+
+    path out_dir = current_path();
+    out_dir += "/DemoOutput_Repose";
+    create_directory(out_dir);
+
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 140; i++) {
+        char filename[200], meshfile[200];
+        sprintf(filename, "%s/DEMdemo_output_%04d.csv", out_dir.c_str(), i);
+        sprintf(meshfile, "%s/DEMdemo_funnel_%04d.vtk", out_dir.c_str(), i);
+        DEMSim.WriteSphereFile(std::string(filename));
+        DEMSim.WriteMeshFile(std::string(meshfile));
+        std::cout << "Frame: " << i << std::endl;
+        DEMSim.DoDynamics(1e-1);
+        DEMSim.ShowThreadCollaborationStats();
+    }
+
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_sec = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+    std::cout << time_sec.count() << " seconds (wall time) to finish the simulation" << std::endl;
+
+    DEMSim.ShowTimingStats();
+    DEMSim.ClearTimingStats();
+
+    std::cout << "DEMdemo_Repose exiting..." << std::endl;
+    return 0;
+}
