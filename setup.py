591e27c974600dd9c05f4cf17de51249d9fb32dc
Ruochun Zhang <ruochunz@gmail.com>
2025-08-15 11:44:35 +0800

    Update setuptools to 61

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="deme",
-    version="2.1.2",
+    version="2.1.3",
     author="Ruochun Zhang",
     author_email="ruochunz@gmail.com",
     description="PyBind Wrapper Library for DEM-Engine",
