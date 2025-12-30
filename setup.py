7d004273d5a6d11de7617532a6fe2bcafa33218b
Ruochun <ruochunz@gmail.com>
2025-12-30 20:47:50 +0800

    Update to 2.1.8

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="deme",
-    version="2.1.7",
+    version="2.1.8",
     author="Ruochun Zhang",
     author_email="ruochunz@gmail.com",
     description="PyBind Wrapper Library for DEM-Engine",
