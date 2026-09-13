b042c6b8e57ccf9f63ff88050b73a804776b2424
Ruochun <ruochunz@gmail.com>
2026-09-13 11:26:58 +0800

    Modify a suspicious C++ std choice

--- src/demo/DEMdemo_SingleSphereCollide.cpp
+++ src/demo/DEMdemo_SingleSphereCollide.cpp
@@ -124,7 +124,7 @@ int main() {
     // Testing modifying jitify options and force model prerequisites
     auto jitify_options = DEMSim.GetJitifyOptions();
     jitify_options.pop_back();  // Remove C++ std17 option
-    jitify_options.push_back("-std=c++20");
+    jitify_options.push_back("-std=c++14");
     DEMSim.SetJitifyOptions(jitify_options);  // Then set it
     my_force_model->DefineCustomModelPrerequisites(
         "float3 __device__ GetContactForce(float3 AOwner, float3 BOwner, float3 ALinVel, float3 BLinVel, "
